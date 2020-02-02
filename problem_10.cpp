2cb0210c0a59a1f65a29b60ef34fa0b4fd53d3e2
PRADUMANSHARMA-180891 <60418558+PRADUMANSHARMA-180891@users.noreply.github.com>
2020-02-02 22:50:32 +0530

    Add files via upload

---
+++ problem_10.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class complex
+{
+	int a,b;
+	public:
+		complex(int x,int y)
+		{
+			a=x;
+			b=y;
+		}
+		complex(int p)
+		{
+			a=p;
+		}
+		complex()
+		{ 
+			a=0;
+			b=0;
+		}
+		void display(void)
+		{
+			cout<<"a ="<<a<<endl;
+			cout<<"b ="<<b<<endl;
+		}
+	
+		
+};
+int main()
+{
+	complex c1(1,3),c2(4),c3;
+	c1.display();
+	c2.display();
+	c3.display();
+	return 0;
+
+}
+
